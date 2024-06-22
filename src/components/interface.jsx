144c9d220af71d27a5baa7e2a10312e763687501
benyou10 <ybenzian016@gmail.com>
2024-06-22 16:12:49 +0100

    video loading 3 fixed

--- src/components/interface.jsx
+++ src/components/interface.jsx
@@ -153,32 +153,38 @@ level :90
   },
 
 ]
+
+
 const Video = ({ src, isLoading }) => {
   const videoRef = useRef();
   return (
     <div>
       {
-        !isLoading && (<div role="status" class="flex items-center justify-center h-56 w-[90%] bg-gray-300 rounded-lg animate-pulse dark:bg-gray-700">
-        <svg class=" h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
-        <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z"/>
-        <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM9 13a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v2Zm4 .382a1 1 0 0 1-1.447.894L10 13v-2l1.553-1.276a1 1 0 0 1 1.447.894v2.764Z"/>
-      </svg>
-        <span class="sr-only">Loading...</span>
-    </div>)&& (
+        isLoading ? (
+          <div role="status" className="flex items-center justify-center h-56 w-[90%] bg-gray-300 rounded-lg animate-pulse dark:bg-gray-700">
+            <svg className="h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
+              <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z"/>
+              <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM9 13a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v2Zm4 .382a1 1 0 0 1-1.447.894L10 13v-2l1.553-1.276a1 1 0 0 1 1.447.894v2.764Z"/>
+            </svg>
+            <span className="sr-only">Loading...</span>
+          </div>
+        ) : (
           <video
-          preload={true}
-          className='rounded-xl max-w-[90%] ' autoPlay loop
+            preload="auto"
+            className="rounded-xl max-w-[90%]"
+            autoPlay
+            loop
             ref={videoRef}
-            
-          
           >
-            <source src={src}  />
+            <source src={src} type="video/mp4" />
           </video>
         )
       }
     </div>
   );
 };
+
+
 
 export function Interface(props) {
 
@@ -466,7 +472,7 @@ visible :{
     </div>
     
        </div>
-       {videoLoaded  || <Skeleton/> }
+     
       
       {/* Actual video element */}
       <Video src="2024-03-29 16-43-47.mp4" />
