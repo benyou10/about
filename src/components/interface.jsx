3bfb7a9484ee7c41d88d95492905367f9e6990b8
benyou10 <ybenzian016@gmail.com>
2024-06-22 16:04:12 +0100

    video loader 3

--- src/components/interface.jsx
+++ src/components/interface.jsx
@@ -158,7 +158,13 @@ const Video = ({ src, isLoading }) => {
   return (
     <div>
       {
-        !isLoading && <Skeleton containerClassName="flex-1" count={5} height={100} /> && (
+        !isLoading && (<div role="status" class="flex items-center justify-center h-56 w-[90%] bg-gray-300 rounded-lg animate-pulse dark:bg-gray-700">
+        <svg class=" h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
+        <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z"/>
+        <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM9 13a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v2Zm4 .382a1 1 0 0 1-1.447.894L10 13v-2l1.553-1.276a1 1 0 0 1 1.447.894v2.764Z"/>
+      </svg>
+        <span class="sr-only">Loading...</span>
+    </div>)&& (
           <video
           preload={true}
           className='rounded-xl max-w-[90%] ' autoPlay loop
@@ -463,13 +469,8 @@ visible :{
        {videoLoaded  || <Skeleton/> }
       
       {/* Actual video element */}
-      <video
-        src="2024-03-29 16-43-47.mp4"
-        className="rounded-xl max-w-[90%]"
-        autoPlay
-        loop
-        onLoadedData={handleVideoLoaded}
-      ></video>
+      <Video src="2024-03-29 16-43-47.mp4" />
+     
 
     <div class=" flex items-start justify-center bac4 text-white">
     <div class="flex flex-col items-start justify-start p-5 text-white">
@@ -528,8 +529,7 @@ visible :{
   <div class="grid xl:grid-cols-2 grid-cols-1 gap-5 h-full ">
     {window.innerWidth < 1000 ? (<> 
       <div class="bg-white flex items-start justify-center  bac1 text-white">
-<video src="2024-06-10 15-31-47.mp4" className='rounded-xl max-w-[90%] ' autoPlay loop></video>
-
+<Video src="2024-06-10 15-31-47.mp4"/>
 </div>
     
 <div class=" flex items-start justify-center bac4 text-white">
@@ -592,7 +592,7 @@ visible :{
        </div>   
     </div>
    <div class="bg-white flex items-start justify-center bac2 text-white">
-<video src="2024-06-10 16-37-01.mp4" className='rounded-xl max-w-[90%] ' autoPlay loop></video>
+<Video src="2024-06-10 16-37-01.mp4"/>
 </div>
 <div class=" flex items-start justify-center bac4 text-white">
     <div class="flex flex-col items-start justify-start p-5 text-white">
@@ -691,8 +691,7 @@ visible :{
        </div>   
     </div>
 <div class="bg-white flex items-start justify-center  bac1 text-white">
-<video src="2024-06-10 15-31-47.mp4" className='rounded-xl max-w-[90%] ' autoPlay loop></video>
-
+<Video src="2024-06-10 15-31-47.mp4"/>
 </div>
 <div class=" flex items-start justify-center bac4 text-white">
     <div class="flex flex-col items-start justify-start p-5 text-white">
@@ -731,7 +730,7 @@ visible :{
        </div>   
     </div>
 <div class="bg-white flex items-start justify-center bac2 text-white">
-<video src="2024-06-10 16-37-01.mp4" className='rounded-xl max-w-[90%] ' autoPlay loop></video>
+<Video src="2024-06-10 16-37-01.mp4"/>
 </div></>)}
  
   
